65ccea169f75a420ae01226b6386a1eb35f2ebb2
shannon-h <hanshann@sheridancollege.ca>
2020-05-29 13:55:49 -0400

    added member id with getters and setters

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -9,6 +9,16 @@ public class Student {
     
     private String name;
 
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public Student(String name) {
         this.name = name;
     }
